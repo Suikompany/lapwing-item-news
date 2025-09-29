94140b094b45a2a4adc899841e216e2e25f22038
HiroM320 <56062913+HiroM320@users.noreply.github.com>
2025-09-30 00:45:07 +0900

    add debug logs

--- src/index.ts
+++ src/index.ts
@@ -7,23 +7,27 @@ import { ALLOW_TWEET } from "./param/envParam";
 const main = async () => {
   const startScrapedAt = truncateUnderMin(new Date());
 
+  // 前回の商品ID一覧を取得
+  const { product_ids: prevProductIdList } = await getData();
+
   // 今回の商品一覧（最新が1番目）を取得
   const productList = await scrapeProductList();
 
-  // 前回の商品ID一覧を取得
-  const { product_ids: prevProductIdList } = await getData();
+  console.debug("productList:", JSON.stringify(productList, null, 2));
 
   // 今回新たに見つかった商品一覧を求める
   const newProductList = productList.filter(
     ({ id }) => !prevProductIdList.includes(id),
   );
 
+  console.debug("newProductList:", JSON.stringify(newProductList, null, 2));
+
   // 新しい商品が一つもない場合は早期終了
   if (newProductList.length < 1) {
     return;
   }
 
-  // 最新の商品一覧で更新
+  // 今回の商品一覧で更新
   await putData(
     startScrapedAt,
     productList.map(({ id }) => id),
