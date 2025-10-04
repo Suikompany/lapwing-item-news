b06dc738ae1ace2abe44f3bc7965f66b6f6e501d
HiroM320 <56062913+HiroM320@users.noreply.github.com>
2025-10-04 19:44:01 +0900

    Lambda関数実行ロールのs3の権限を調整

--- terraform/module/lambda.tf
+++ terraform/module/lambda.tf
@@ -86,11 +86,13 @@ resource "aws_iam_policy" "lambda_policy" {
       {
         Effect = "Allow"
         Action = [
-          "s3:PutObject",
+          "s3:ListBucket",
           "s3:GetObject",
+          "s3:PutObject",
         ],
         Resource = [
-          "${aws_s3_bucket.bucket.arn}/*"
+          "${aws_s3_bucket.bucket.arn}",
+          "${aws_s3_bucket.bucket.arn}/*",
         ]
       },
       {
